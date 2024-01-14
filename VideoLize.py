e2d46687db63879462a49bd666547638fd4a1cb7
avisek <avisekksarma@gmail.com>
2024-01-14 09:34:39 +0545

    made some minor ui changes

--- VideoLize.py
+++ VideoLize.py
@@ -21,7 +21,7 @@ if selected == "Home":
 
     # utility functions
     def display_video():
-        video_file = open('example_videos/butterfly.mp4', 'rb')
+        video_file = open('example_videos/star-lifecycle.mp4', 'rb')
         video_bytes = video_file.read()
         return video_bytes
 
@@ -30,8 +30,8 @@ if selected == "Home":
         count = 0
         my_bar = st.progress(count, text=progress_text)
 
-        for percent_complete in range(100):
-            time.sleep(0.5)
+        for _ in range(100):
+            time.sleep(0.1)
             count += 1
             my_bar.progress(count, text=progress_text)
         time.sleep(1)
@@ -56,10 +56,10 @@ if selected == "Home":
 
 
         for i in range(st.session_state.c):
-            c1, c2 = st.columns(2)
-            with c1:
-                st.text_input(f"Scene {i}", key=f"text{i}")
-
+            # c1, c2 = st.columns(2)
+            # with c1:
+            #     st.text_input(f"Scene {i}", key=f"text{i}")
+            st.text_input(f"Scene {i+1}", key=f"text{i+1}")
 
         def collect():
             st.session_state.prompts = []
@@ -116,7 +116,7 @@ if selected == "Examples":
         'Evolution': ["A chimpanzee in African jungle|Homo habilis holding stone tools in African jungle|Two Archaic Homo Sapiens interacting with each other|A modern human farming in a farmland|A modern human wearing suit in a city with a lot of buildings|A modern human using computers and AI", "Human evolution is a continuous process that spans millions of years, involving biological adaptations and cultural developments that have shaped Homo sapiens into the species we are today.", "example_videos/evolution.mp4"],
         'Butterfly': ["A yellow butterfly lays egg on a green leaf|Very very tiny egg of butterfly is kept on the green leaves|Caterpillar eats leaves and grows|Green Pupa hangs gently from the branch|A vibrant yellow butterfly emerges from the pupa, ready to dance in the sky.", "In a whimsical dance of nature, a yellow butterfly delicately lays tiny eggs on leaves. These eggs hatch into voracious caterpillars, voraciously munching on leaves until they undergo a miraculous transformation within protective pupae. The grand finale reveals a newly emerged butterfly, symbolizing the captivating cycle of life and metamorphosis", "example_videos/butterfly.mp4"],
         'Stellar Evolution': ["Empty and dark space in the universe| Stellar nebula| A star grows into an extremely large red giant| A spherical red old sun in space| A burning young star| A star goes supernova| Black-filled hole, black hole, circle, sphere", "The star lifecycle begins with a gaseous nebula, a vast cloud of gas and dust in space. Under the influence of gravity, the nebula begins to collapse, giving birth to a young star. As the star matures, it enters the phase of a red giant with the expansion of its outer layers. Massive stars undergo supernova, releasing an immense amount of energy and scattering elements into space. The explosion can lead to the formation of a black hole, an incredibly dense region where gravity is so strong that nothing, not even light, can escape.", "example_videos/star-lifecycle.mp4"],
-        "World War II": ["US Airplane flies on the sky carrying little boy (bomb used in second world war)|The US airplane drops the bomb on hiroshima.|Bomb creates a huge explosion destroying building in its perimeter in Hiroshima and produces large amount of radiation.|People in the perimeter of explosion turn into ashes.|Eventually, the people who suffer from radiation die due to radiation.", "In WWII, a US plane dropped the 'Little Boy' bomb on Hiroshima, causing a massive explosion and releasing lethal radiation. The immediate impact turned people to ashes, while long-term exposure led to fatal radiation-related illnesses. This tragic event left an enduring legacy, illustrating the devastating toll of atomic warfar", "example_videos/world-war.mp4"],
+        "World War II": ["US Airplane flies on the sky carrying little boy (bomb used in second world war)|The US airplane drops the bomb on hiroshima.|Bomb creates a huge explosion destroying building in its perimeter in Hiroshima and produces large amount of radiation.|People in the perimeter of explosion turn into ashes.|Eventually, the people who suffer from radiation die due to radiation.", "In WWII, a US plane dropped the 'Little Boy' bomb on Hiroshima, causing a massive explosion and releasing lethal radiation. The immediate impact turned people to ashes, while long-term exposure led to fatal radiation-related illnesses. This tragic event left an enduring legacy, illustrating the devastating toll of atomic warfar", "example_videos/world_war.mp4"],
         "Abstract Art": ["Abstract human face, happy geometric, colorful|Random geometric abstract art|Abstract human face, happy geometric, colorful|Random geometric abstract art|Abstract human face, happy geometric, colorful|Random geometric abstract art|Abstract human face, happy geometric, colorful|Random geometric abstract art|Abstract human face, happy geometric, colorful", "I've wondered that I've never go the hunger to fall asleep only after seeing my dreams. A mixture of miscible fluids wit and wisdom drinking the night off.Now sleepless it remains.What fun we had! Nothing went as bad.As blame We even laughed the same-like a real, true rhyme.I wish we'd had more time.", "example_videos/abstract-art.mp4"]
     }
 
